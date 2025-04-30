2717a353305d887b4a1307120778fc3badcebea5
Juny7736 <handaniel721@gmail.com>
2025-04-30 19:24:40 -0400

    remove ./ for image path

--- junseohanPW/src/components/sections/Hero.tsx
+++ junseohanPW/src/components/sections/Hero.tsx
@@ -168,7 +168,7 @@ const Hero = () => {
                   }}
                 />
                 <img
-                  src="../images/linkedinpic.jpg"
+                  src="images/linkedinpic.jpg"
                   alt="Junseo Han - Profile Picture"
                   className="absolute inset-0 w-full h-full object-cover mix-blend-overlay"
                 />
